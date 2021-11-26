371e33558efe75ee95a0667d21d10dd3c979ed1c
holozeros <63087573+holozeros@users.noreply.github.com>
2021-11-27 08:19:46 +0900

    Update build_chroot_environment.sh

--- Chroot environment/build_chroot_environment.sh
+++ Chroot environment/build_chroot_environment.sh
@@ -427,17 +427,11 @@ chroot "$LFS" /usr/bin/env -i   \
     PS1='(lfs chroot) \u:\w\$ ' \
     PATH=/usr/bin:/usr/sbin     \
     /bin/bash --login +h
-umount -v $LFS/dev/pts
-umount -v $LFS/dev
-umount -v $LFS/sys
-umount -v $LFS/proc
-umount -v $LFS/run
-
-
+umount -v $LFS/dev{/pts,}
+umount -v $LFS/{sys,proc,run}
 ####################
 ### creating dir ###
 ####################
-
 mkdir -pv /{boot,home,mnt,opt,srv}
 mkdir -pv /etc/{opt,sysconfig}
 mkdir -pv /lib/firmware
@@ -454,12 +448,10 @@ ln -sfv /run/lock /var/lock
 install -dv -m 0750 /root
 install -dv -m 1777 /tmp /var/tmp
 ln -sv /proc/self/mounts /etc/mtab
-
 cat > /etc/hosts << EOF
 127.0.0.1  localhost $(hostname)
 ::1        localhost
 EOF
-
 cat > /etc/passwd << "EOF"
 root:x:0:0:root:/root:/bin/bash
 bin:x:1:1:bin:/dev/null:/bin/false
@@ -468,7 +460,6 @@ messagebus:x:18:18:D-Bus Message Daemon User:/run/dbus:/bin/false
 uuidd:x:80:80:UUID Generation Daemon User:/dev/null:/bin/false
 nobody:x:99:99:Unprivileged User:/dev/null:/bin/false
 EOF
-
 cat > /etc/group << "EOF"
 root:x:0:
 bin:x:1:daemon
@@ -496,18 +487,14 @@ wheel:x:97:
 nogroup:x:99:
 users:x:999:
 EOF
-
 echo "tester:x:101:101::/home/tester:/bin/bash" >> /etc/passwd
 echo "tester:x:101:" >> /etc/group
 install -o tester -d /home/tester
-
 exec /bin/bash --login +h
-
 touch /var/log/{btmp,lastlog,faillog,wtmp}
 chgrp -v utmp /var/log/lastlog
 chmod -v 664  /var/log/lastlog
 chmod -v 600  /var/log/btmp
-
 #######################
 ### libstdc++ pass2 ###
 #######################
@@ -613,454 +600,100 @@ make
 make install
 cd ..
 rm -rf util-linux-2.37.2
-
->>>>>>>>
-
-
-
+################
+### striping ###
+################
+rm -rf /usr/share/{info,man,doc}/*
+find /usr/{lib,libexec} -name \*.la -delete
+rm -rf /tools
+##############
+### backup ###
+##############
+exit
+umount $LFS/dev{/pts,}
+umount $LFS/{sys,proc,run}
+rm /tools
+export LFS=/mnt/lfs
+cd $LFS 
+tar -cJpf $HOME/lfs-temp-tools-11.0.tar.xz .
+###############
+### restore ###
+###############
+su -
+export LFS=/mnt/lfs
+cd $LFS 
+rm -rf ./* 
+tar -xpf $HOME/lfs-temp-tools-11.0.tar.xz
 #################
 ### tcl8.6.11 ###
 #################
-
 tar xf tcl8.6.11-src.tar.gz
 cd tcl8.6.11
-
 cd unix
-./configure --prefix=/tools
+./configure --prefix=/usr
 make
 # TZ=UTC make test
 make install
-chmod -v u+w /tools/lib/libtcl8.6.so
+chmod -v u+w /usr/lib/libtcl8.6.so
 make install-private-headers
-ln -sv tclsh8.6 /tools/bin/tclsh
-
+ln -sv tclsh8.6 /usr/bin/tclsh
 cd ../..
 rm -rf tcl8.6.11
-
 ####################
 ### expect5.45.4 ###
 ####################
-
 tar xf expect5.45.4.tar.gz
 cd expect5.45.4
-
 cp -v configure{,.orig}
 sed 's:/usr/local/bin:/bin:' configure.orig > configure
-./configure --prefix=/tools       \
-            --with-tcl=/tools/lib \
-            --with-tclinclude=/tools/include
+./configure --prefix=/usr         \
+            --with-tcl=/usr/lib   \
+            --with-tclinclude=/usr/include
 make
 make SCRIPTS="" install
-ln -s /tools/lib/expect5.45.4/libexpect5.45.4.so /tools/lib/libexpect-5.45.4.so
-
+ln -s /usr/lib/expect5.45.4/libexpect5.45.4.so /usr/lib/libexpect-5.45.4.so
 cd ..
 rm -rf expect5.45.4
-
 #####################
 ### dejagnu-1.6.3 ###
 #####################
-
 tar xf dejagnu-1.6.3.tar.gz
 cd dejagnu-1.6.3
-
 mkdir -v build
 cd       build
-
-../configure --prefix=/tools
+../configure --prefix=/usr
 make install
-
 cd ../..
 rm -rf dejagnu-1.6.3
-
 ####################
 ### check-0.15.2 ###
 ####################
-
 tar xf check-0.15.2.tar.gz
 cd check-0.15.2
-
-PKG_CONFIG= ./configure --prefix=/tools
+PKG_CONFIG= ./configure --prefix=/usr
 make
 make install
-
 cd ..
 rm -rf check-0.15.2
-
-###################
-### ncurses-6.2 ###
-###################
-
-tar xf ncurses-6.2.tar.gz
-cd ncurses-6.2
-
-sed -i s/mawk// configure
-mkdir build
-pushd build
-  ../configure
-  make -C include
-  make -C progs tic
-popd
-./configure --prefix=/tools              \
-            --mandir=/tools/share/man    \
-            --with-shared                \
-            --without-debug              \
-            --without-ada                \
-            --without-normal             \
-            --enable-widec
-make
-make TIC_PATH=$(pwd)/build/progs/tic install
-echo "INPUT(-lncursesw)" > /tools/lib/libncurses.so
-ln -sr /tools/include/ncursesw/* /tools/include
-
-cd ..
-rm -rf ncurses-6.2
-
-##################
-### bash-5.1.8 ###
-##################
-tar xf bash-5.1.8.tar.gz
-cd bash-5.1.8
-
-./configure --prefix=/tools                 \
-            --without-bash-malloc
-make
-make install
-ln -sv bash /tools/bin/sh 
-
-cd ..
-rm -rf bash-5.1.8
-
 ###################
 ### bzip2-1.0.8 ###
 ###################
-
 tar xf bzip2-1.0.8.tar.gz
 cd bzip2-1.0.8
-
 patch -Np1 -i ../bzip2-1.0.8-install_docs-1.patch
 sed -i 's@\(ln -s -f \)$(PREFIX)/bin/@\1@' Makefile
 sed -i "s@(PREFIX)/man@(PREFIX)/share/man@g" Makefile
 make -f Makefile-libbz2_so
 make clean
 make
-make PREFIX=/tools install
-cp -av libbz2.so.* /tools/lib
-ln -sv libbz2.so.1.0.8 /tools/lib/libbz2.so
-cp -v bzip2-shared /tools/bin/bzip2
-for i in /tools/bin/{bzcat,bunzip2}; do
+make PREFIX=/usr install
+cp -av libbz2.so.* /usr/lib
+ln -sv libbz2.so.1.0.8 /usr/lib/libbz2.so
+cp -v bzip2-shared /usr/bin/bzip2
+for i in /usr/bin/{bzcat,bunzip2}; do
   ln -sfv bzip2 $i
 done
-rm -fv /tools/lib/libbz2.a
-
+rm -fv /usr/lib/libbz2.a
 cd ..
 rm -rf bzip2-1.0.8
-
-######################
-### coreutils-8.32 ###
-######################
-
-tar xf coreutils-8.32.tar.xz
-cd coreutils-8.32
-
-./configure --prefix=/tools                   \
-            --enable-install-program=hostname
-make
-# make RUN_EXPENSIVE_TESTS=yes check
-make install
-mv -v /tools/bin/chroot /tools/sbin
-mkdir -pv /tools/share/man/man8
-mv -v /tools/share/man/man1/chroot.1 /tools/share/man/man8/chroot.8
-sed -i 's/"1"/"8"/' /tools/share/man/man8/chroot.8
-
-cd ../
-rm -rf coreutils-8.32
-
-#####################
-### diffutils-3.8 ###
-#####################
-
-tar xf diffutils-3.8.tar.xz
-cd diffutils-3.8
-
-./configure --prefix=/tools
-make
-# make check
-make install
-
-cd ..
-rm -rf diffutils-3.8
-
-#################
-### file-5.40 ###
-#################
-
-tar xf file-5.40.tar.gz
-cd file-5.40
-
-./configure --prefix=/tools
-make
-make install
-
-cd ..
-rm -rf file-5.40
-
-#######################
-### findutils-4.8.0 ###
-#######################
-
-tar xf findutils-4.8.0.tar.xz
-cd findutils-4.8.0
-
-./configure --prefix=/tools
-make
-# make check
-make install
-
-cd ..
-rm -rf findutils-4.8.0
-
-##################
-### gawk-5.1.0 ###
-##################
-
-tar xf gawk-5.1.0.tar.xz
-cd gawk-5.1.0
-
-./configure --prefix=/tools
-make
-make install
-
-cd ..
-rm -rf gawk-5.1.0
-
-####################
-### gettext-0.21 ###
-####################
-
-tar xf gettext-0.21.tar.xz
-cd gettext-0.21
-
-EMACS="no" ./configure --prefix=/tools --disable-shared
-make
-
-cp -v gettext-tools/src/{msgfmt,msgmerge,xgettext} /tools/bin
-
-cd ..
-rm -rf gettext-0.21
-
-################
-### grep-3.7 ###
-################
-
-tar xf grep-3.7.tar.xz
-cd grep-3.7
-
-./configure --prefix=/tools
-make
-# make check
-make install
-
-cd ..
-rm -rf grep-3.7
-
-#################
-### gzip-1.10 ###
-#################
-
-tar xf gzip-1.10.tar.xz
-cd gzip-1.10
-
-./configure --prefix=/tools
-make
-# make check
-make install
-
-cd ..
-rm -rf gzip-1.10
-
-#################
-### m4-1.4.19 ###
-#################
-
-tar xf m4-1.4.19.tar.xz
-cd m4-1.4.19
-
-./configure --prefix=/tools
-make
-# make check
-make install
-
-cd ..
-rm -rf m4-1.4.19
-
-################
-### make-4.3 ###
-################
-
-tar xf make-4.3.tar.gz
-cd make-4.3
-
-./configure --prefix=/tools --without-guile
-make
-# make check
-make install
-
-cd ..
-rm -rf make-4.3
-
-###################
-### patch-2.7.6 ###
-###################
-
-tar xf patch-2.7.6.tar.xz
-cd patch-2.7.6
-
-./configure --prefix=/tools
-make
-# make check
-make install
-
-cd ..
-rm -rf patch-2.7.6
-
-###################
-### perl-5.34.0 ###
-###################
-
-tar xf perl-5.34.0.tar.xz
-cd perl-5.34.0
-
-sh Configure -des                                          \
-             -Dprefix=/tools                               \
-             -Dlibs=-lm                                    \
-             -Dvendorprefix=/tools                         \
-             -Dprivlib=/tools/lib/perl5/5.34/core_perl     \
-             -Darchlib=/tools/lib/perl5/5.34/core_perl     \
-             -Dsitelib=/tools/lib/perl5/5.34/site_perl     \
-             -Dsitearch=/tools/lib/perl5/5.34/site_perl    \
-             -Dvendorlib=/tools/lib/perl5/5.34/vendor_perl \
-             -Dvendorarch=/tools/lib/perl5/5.34/vendor_perl
-make
-# make test
-make install
-cp -v perl cpan/podlators/scripts/pod2man /tools/bin
-cp -Rv lib/* /tools/lib/perl5/5.34
-
-cd ..
-rm -rf perl-5.34.0
-
-###############
-### sed-4.8 ###
-###############
-
-tar xf sed-4.8.tar.xz
-cd sed-4.8
-
-./configure --prefix=/tools
-make
-# make check
-make install
-
-cd ..
-rm -rf sed-4.8
-
-################
-### tar-1.34 ###
-################
-
-cd $LFS/sources
-tar xf tar-1.34.tar.xz
-cd tar-1.34
-
-./configure --prefix=/tools
-make
-# make check
-make install
-
-cd ..
-rm -rf tar-1.34
-
-####################
-#### texinfo-6.8 ###
-####################
-
-tar xf texinfo-6.8.tar.xz
-cd texinfo-6.8
-
-sed -e 's/__attribute_nonnull__/__nonnull/' \
-    -i gnulib/lib/malloc/dynarray-skeleton.c
-./configure --prefix=/tools
-make
-make install
-
-cd ..
-rm -rf texinfo-6.8
-
-###################
-### bison-3.7.6 ###
-###################
-
-tar xf bison-3.7.6.tar.xz
-cd bison-3.7.6
-
-./configure --prefix=/tools
-make
-# make check
-make install
-
-cd ..
-rm -rf bison-3.7.6
-
-################
-### xz-5.2.5 ###
-################
-
-tar xf xz-5.2.5.tar.xz
-cd xz-5.2.5
-
-./configure --prefix=/tools
-make
-# make check
-make install
-
-cd ..
-rm -rf xz-5.2.5
-
-####################
-### Python-3.9.6 ###
-####################
-
-tar xf Python-3.9.6.tar.xz
-cd Python-3.9.6
-
-./configure --prefix=/tools   \
-            --enable-shared   \
-            --without-ensurepip
-make
-make install
-
-cd ..
-rm -rf Python-3.9.6
-
-#########################
-### util-linux-2.37.2 ###
-#########################
-
-tar xf util-linux-2.37.2.tar.xz
-cd util-linux-2.37.2
-
-mkdir -pv /var/lib/hwclock
-./configure --prefix=/tools                                \
-            ADJTIME_PATH=/var/lib/hwclock/adjtime          \
-            --disable-static                               \
-            --without-systemdsystemunitdir                 \
-            --disable-makeinstall-chown                    \
-            PKG_CONFIG=""
-make
-make install
-
-cd ..
-rm -rf util-linux-2.37.2
-
-END
-```
+EOF
