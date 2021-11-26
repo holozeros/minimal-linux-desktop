ed22a94464e906518e510ac1eb03d765c3ad3393
holozeros <63087573+holozeros@users.noreply.github.com>
2021-11-26 21:35:12 +0000

    Update build_chroot_environment.sh

--- Chroot environment/build_chroot_environment.sh
+++ Chroot environment/build_chroot_environment.sh
@@ -273,7 +273,7 @@ cd gzip-1.10
 make
 make DESTDIR=$LFS install
 cd ..
-rm -rf gzip-3.7
+rm -rf gzip-1.10
 ################
 ### make-4.3 ###
 ################
@@ -433,7 +433,190 @@ umount -v $LFS/sys
 umount -v $LFS/proc
 umount -v $LFS/run
 
-######################################################33
+
+####################
+### creating dir ###
+####################
+
+mkdir -pv /{boot,home,mnt,opt,srv}
+mkdir -pv /etc/{opt,sysconfig}
+mkdir -pv /lib/firmware
+mkdir -pv /media/{floppy,cdrom}
+mkdir -pv /usr/{,local/}{include,src}
+mkdir -pv /usr/local/{bin,lib,sbin}
+mkdir -pv /usr/{,local/}share/{color,dict,doc,info,locale,man}
+mkdir -pv /usr/{,local/}share/{misc,terminfo,zoneinfo}
+mkdir -pv /usr/{,local/}share/man/man{1..8}
+mkdir -pv /var/{cache,local,log,mail,opt,spool}
+mkdir -pv /var/lib/{color,misc,locate}
+ln -sfv /run /var/run
+ln -sfv /run/lock /var/lock
+install -dv -m 0750 /root
+install -dv -m 1777 /tmp /var/tmp
+ln -sv /proc/self/mounts /etc/mtab
+
+cat > /etc/hosts << EOF
+127.0.0.1  localhost $(hostname)
+::1        localhost
+EOF
+
+cat > /etc/passwd << "EOF"
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/dev/null:/bin/false
+daemon:x:6:6:Daemon User:/dev/null:/bin/false
+messagebus:x:18:18:D-Bus Message Daemon User:/run/dbus:/bin/false
+uuidd:x:80:80:UUID Generation Daemon User:/dev/null:/bin/false
+nobody:x:99:99:Unprivileged User:/dev/null:/bin/false
+EOF
+
+cat > /etc/group << "EOF"
+root:x:0:
+bin:x:1:daemon
+sys:x:2:
+kmem:x:3:
+tape:x:4:
+tty:x:5:
+daemon:x:6:
+floppy:x:7:
+disk:x:8:
+lp:x:9:
+dialout:x:10:
+audio:x:11:
+video:x:12:
+utmp:x:13:
+usb:x:14:
+cdrom:x:15:
+adm:x:16:
+messagebus:x:18:
+input:x:24:
+mail:x:34:
+kvm:x:61:
+uuidd:x:80:
+wheel:x:97:
+nogroup:x:99:
+users:x:999:
+EOF
+
+echo "tester:x:101:101::/home/tester:/bin/bash" >> /etc/passwd
+echo "tester:x:101:" >> /etc/group
+install -o tester -d /home/tester
+
+exec /bin/bash --login +h
+
+touch /var/log/{btmp,lastlog,faillog,wtmp}
+chgrp -v utmp /var/log/lastlog
+chmod -v 664  /var/log/lastlog
+chmod -v 600  /var/log/btmp
+
+#######################
+### libstdc++ pass2 ###
+#######################
+tar xf gcc-11.2.0.tar.xz
+cd gcc-11.2.0
+ln -s gthr-posix.h libgcc/gthr-default.h
+mkdir -v build
+cd       build
+../libstdc++-v3/configure            \
+    CXXFLAGS="-g -O2 -D_GNU_SOURCE"  \
+    --prefix=/usr                    \
+    --disable-multilib               \
+    --disable-nls                    \
+    --host=$(uname -m)-lfs-linux-gnu \
+    --disable-libstdcxx-pch
+make
+make install
+cd ../..
+rm -rf gcc-11.2.0
+####################
+### gettext-0.21 ###
+####################
+tar xf gettext-0.21.tar.xz
+cd gettext-0.21
+./configure --disable-shared
+make
+cp -v gettext-tools/src/{msgfmt,msgmerge,xgettext} /usr/bin
+cd ..
+rm -rf gettext-0.21
+###################
+### bison-3.7.6 ###
+###################
+tar xf bison-3.7.6.tar.xz
+cd bison-3.7.6
+./configure --prefix=/usr \
+            --docdir=/usr/share/doc/bison-3.7.6
+make
+make install
+cd ..
+rm -rf bison-3.7.6
+###################
+### perl-5.34.0 ###
+###################
+tar xf perl-5.34.0.tar.xz
+cd perl-5.34.0
+sh Configure -des                                        \
+             -Dprefix=/usr                               \
+             -Dvendorprefix=/usr                         \
+             -Dprivlib=/usr/lib/perl5/5.34/core_perl     \
+             -Darchlib=/usr/lib/perl5/5.34/core_perl     \
+             -Dsitelib=/usr/lib/perl5/5.34/site_perl     \
+             -Dsitearch=/usr/lib/perl5/5.34/site_perl    \
+             -Dvendorlib=/usr/lib/perl5/5.34/vendor_perl \
+             -Dvendorarch=/usr/lib/perl5/5.34/vendor_perl
+make
+make install
+cd ..
+rm -rf perl-5.34.0
+####################
+### python-3.9.6 ###
+####################
+tar xf Python-3.9.6.tar.xz
+cd Python-3.9.6
+./configure --prefix=/usr   \
+            --enable-shared \
+            --without-ensurepip
+make
+make install
+cd ..
+rm -rf Python-3.9.6
+###################
+### texinfo-6.8 ###
+###################
+tar xf texinfo-6.8.tar.xz
+cd texinfo-6.8
+sed -e 's/__attribute_nonnull__/__nonnull/' \
+    -i gnulib/lib/malloc/dynarray-skeleton.c
+./configure --prefix=/usr
+make
+make install
+cd ..
+rm -rf texinfo-6.8
+#########################
+### util-linux-2.37.2 ###
+#########################
+tar xf util-linux-2.37.2.tar.xz
+cd util-linux-2.37.2
+mkdir -pv /var/lib/hwclock
+./configure ADJTIME_PATH=/var/lib/hwclock/adjtime    \
+            --libdir=/usr/lib    \
+            --docdir=/usr/share/doc/util-linux-2.37.2 \
+            --disable-chfn-chsh  \
+            --disable-login      \
+            --disable-nologin    \
+            --disable-su         \
+            --disable-setpriv    \
+            --disable-runuser    \
+            --disable-pylibmount \
+            --disable-static     \
+            --without-python     \
+            runstatedir=/run
+make
+make install
+cd ..
+rm -rf util-linux-2.37.2
+
+>>>>>>>>
+
+
 
 #################
 ### tcl8.6.11 ###
