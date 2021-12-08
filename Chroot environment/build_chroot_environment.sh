8e070f345f093b3480452f2416eb3c3296c61016
holozeros <63087573+holozeros@users.noreply.github.com>
2021-12-08 11:20:23 +0900

    Update build_chroot_environment.sh

--- Chroot environment/build_chroot_environment.sh
+++ Chroot environment/build_chroot_environment.sh
@@ -11,10 +11,14 @@ mkdir -v build
 cd       build
 ../configure --prefix=$LFS/tools \
              --with-sysroot=$LFS \
+             --with-lib-path=$LFS/lib \
              --target=$LFS_TGT   \
              --disable-nls       \
              --disable-werror
 make
+case $(uname -m) in
+  x86_64) mkdir -v /tools/lib && ln -sv lib /tools/lib64 ;;
+esac
 make install -j1
 cd ../..
 rm -rf binutils-2.37
@@ -29,22 +33,41 @@ tar -xf ../gmp-6.2.1.tar.xz
 mv -v gmp-6.2.1 gmp
 tar -xf ../mpc-1.2.1.tar.gz
 mv -v mpc-1.2.1 mpc
+
+for file in gcc/config/{linux,i386/linux{,64}}.h
+do
+  cp -uv $file{,.orig}
+  sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
+      -e 's@/usr@/tools@g' $file.orig > $file
+  echo '
+  
+#undef GLIBC_DYNAMIC_LINKER64
+#undef STANDARD_STARTFILE_PREFIX_1
+#undef STANDARD_STARTFILE_PREFIX_2
+#define GLIBC_DYNAMIC_LINKER64 "/lib64/ld-linux-x86-64.so.2"
+#define STANDARD_STARTFILE_PREFIX_1 "/lib"
+#define STANDARD_STARTFILE_PREFIX_2 "/tools/lib"' >> $file
+  touch $file.orig
+done
+
 case $(uname -m) in
   x86_64)
     sed -e '/m64=/s/lib64/lib/' \
         -i.orig gcc/config/i386/t-linux64
  ;;
 esac
+
 mkdir -v build
 cd       build
 ../configure                                       \
     --target=$LFS_TGT                              \
-    --prefix=$LFS/tools                            \
+    --prefix=/tools                                \
     --with-glibc-version=2.11                      \
     --with-sysroot=$LFS                            \
     --with-newlib                                  \
     --without-headers                              \
-    --enable-initfini-array                        \
+    --with-local-prefix=/tools                     \
+    --with-native-system-header-dir=/tools/include \
     --disable-nls                                  \
     --disable-shared                               \
     --disable-multilib                             \
@@ -52,17 +75,19 @@ cd       build
     --disable-threads                              \
     --disable-libatomic                            \
     --disable-libgomp                              \
+    --disable-libmpx                               \
     --disable-libquadmath                          \
     --disable-libssp                               \
     --disable-libvtv                               \
     --disable-libstdcxx                            \
     --enable-languages=c,c++
+
 make
 make install
-cd ..
-cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
-  `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/install-tools/include/limits.h
-cd ..
+#cd ..
+#cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+#  `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/install-tools/include/limits.h
+cd ../..
 rm -rf gcc-11.2.0
 #################################
 ### linux-api-headers-5.13.12 ###
@@ -73,7 +98,7 @@ make mrproper
 make headers
 find usr/include -name '.*' -delete
 rm usr/include/Makefile
-cp -rv usr/include $LFS/usr
+cp -rv usr/include $LFS/
 cd ..
 rm -rf linux-5.13.12
 ##################
@@ -91,22 +116,23 @@ esac
 patch -Np1 -i ../glibc-2.34-fhs-1.patch
 mkdir -v build
 cd       build
-echo "rootsbindir=/usr/sbin" > configparms
 ../configure                             \
-      --prefix=/usr                      \
+      --prefix=/                         \
       --host=$LFS_TGT                    \
       --build=$(../scripts/config.guess) \
       --enable-kernel=3.2                \
-      --with-headers=$LFS/usr/include    \
-      libc_cv_slibdir=/usr/lib
-make -j1
+      --with-headers=$LFS/include        \
+      libc_cv_forced_unwind=yes          \
+      libc_cv_c_cleanup=yes
+make
 make DESTDIR=$LFS install
-sed '/RTLDLIST=/s@/usr@@g' -i $LFS/usr/bin/ldd
-$LFS_TGT-gcc dummy.c
+
+echo 'int main(){}' > dummy.c
+$LFS_TGT-gcc -B/mnt/lfs/lib dummy.c
 readelf -l a.out | grep '/ld-linux'
   # [Requesting program interpreter: /lib64/ld-linux-x86-64.so.2]
 rm -v dummy.c a.out
-$LFS/tools/libexec/gcc/$LFS_TGT/11.2.0/install-tools/mkheaders
+# $LFS/tools/libexec/gcc/$LFS_TGT/11.2.0/install-tools/mkheaders
 cd ../..
 rm -rf glibc-2.34
 ##############################
@@ -118,12 +144,12 @@ mkdir -v build
 cd       build
 ../libstdc++-v3/configure           \
     --host=$LFS_TGT                 \
-    --build=$(../config.guess)      \
-    --prefix=/usr                   \
+    --prefix=/tools                 \
     --disable-multilib              \
     --disable-nls                   \
+    --disable-libstdcxx-threads     \
     --disable-libstdcxx-pch         \
-    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/11.2.0
+    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/7.3.0
 make
 make DESTDIR=$LFS install
 cd ../..
