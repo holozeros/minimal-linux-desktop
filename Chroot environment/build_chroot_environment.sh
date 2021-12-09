6b492fe2f228544e5a216bba893c95bd0db92d76
holozeros <63087573+holozeros@users.noreply.github.com>
2021-12-10 00:09:42 +0900

    Update build_chroot_environment.sh

--- Chroot environment/build_chroot_environment.sh
+++ Chroot environment/build_chroot_environment.sh
@@ -173,6 +173,85 @@ make
 make install
 cd ../..
 rm -rf gcc-11.2.0
+######################
+### binutils pass2 ###
+######################
+tar xf binutils-2.37.tar.xz 
+cd binutils-2.37
+mkdir -v build
+cd       build
+CC=$LFS_TGT-gcc                \
+AR=$LFS_TGT-ar                 \
+RANLIB=$LFS_TGT-ranlib         \
+../configure                   \
+    --prefix=/tools            \
+    --disable-nls              \
+    --disable-werror           \
+    --with-lib-path=/tools/lib \
+    --with-sysroot
+make
+make install
+make -C ld clean
+make -C ld LIB_PATH=/usr/lib:/lib
+cp -v ld/ld-new /tools/bin
+cd ../..
+rm -rf  binutils-2.37
+#################
+### gcc pass2 ###
+#################
+tar xf gcc-11.2.0.tar.xz
+cd gcc-11.2.0
+cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+  `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include-fixed/limits.h
+for file in gcc/config/{linux,i386/linux{,64}}.h
+do
+  cp -uv $file{,.orig}
+  sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
+      -e 's@/usr@/tools@g' $file.orig > $file
+  echo '
+#undef STANDARD_STARTFILE_PREFIX_1
+#undef STANDARD_STARTFILE_PREFIX_2
+#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
+#define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
+  touch $file.orig
+done
+case $(uname -m) in
+  x86_64)
+    sed -e '/m64=/s/lib64/lib/' \
+        -i.orig gcc/config/i386/t-linux64
+  ;;
+esac
+tar -xf ../mpfr-3.1.5.tar.xz
+mv -v mpfr-3.1.5 mpfr
+tar -xf ../gmp-6.1.2.tar.xz
+mv -v gmp-6.1.2 gmp
+tar -xf ../mpc-1.0.3.tar.gz
+mv -v mpc-1.0.3 mpc
+mkdir -v build
+cd       build
+CC=$LFS_TGT-gcc                                    \
+CXX=$LFS_TGT-g++                                   \
+AR=$LFS_TGT-ar                                     \
+RANLIB=$LFS_TGT-ranlib                             \
+../configure                                       \
+    --prefix=/tools                                \
+    --with-local-prefix=/tools                     \
+    --with-native-system-header-dir=/tools/include \
+    --enable-languages=c,c++                       \
+    --disable-libstdcxx-pch                        \
+    --disable-multilib                             \
+    --disable-bootstrap                            \
+    --disable-libgomp
+make
+make install
+ln -sv gcc /tools/bin/cc
+echo 'int main(){}' > dummy.c
+cc dummy.c
+readelf -l a.out | grep ': /tools'
+  #[Requesting program interpreter: /tools/lib/ld-linux.so.2]
+rm -v dummy.c a.out
+cd ../...
+rm -rf gcc-11.2.0
 ##########
 ### m4 ###
 ##########
