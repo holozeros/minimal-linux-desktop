df7489190ef9726550f67e713006305812d4e06d
holozeros <63087573+holozeros@users.noreply.github.com>
2021-12-09 23:00:11 +0900

    Update build_chroot_environment.sh

--- Chroot environment/build_chroot_environment.sh
+++ Chroot environment/build_chroot_environment.sh
@@ -104,9 +104,9 @@ rm -rf linux-5.13.12
 tar xf glibc-2.34.tar.xz
 cd glibc-2.34
 patch -Np1 -i ../glibc-2.34-fhs-1.patch
-echo "rootsbindir=/sbin" > configparms
 mkdir -v build
 cd       build
+echo "rootsbindir=/sbin" > configparms
 ../configure                             \
       --prefix=/tools                    \
       --host=$LFS_TGT                    \
@@ -114,8 +114,8 @@ cd       build
       --enable-kernel=3.2                \
       libc_cv_forced_unwind=yes          \
       libc_cv_c_cleanup=yes              \
-      --with-headers=/tools/include
-#      libc_cv_slibdir=/tools/lib
+      --with-headers=/tools/include      \
+      libc_cv_slibdir=/tools/lib
 make
 make install
 echo 'int main(){}' > dummy.c
